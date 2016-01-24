d2e0d469d7094991a1fc1ecda2e0a814a7a0db5b
Kyle Lee <kylekclee@gmail.com>
2016-01-24 05:21:05 -0800

    adding script to parse site for parking data

---
+++ parseSDOTsite.py
@@ -0,0 +1,30 @@
+import urllib2
+import csv
+#python 2
+start_date = '01012015'
+end_date ='01072015'
+url = 'http://web6.seattle.gov/SDOT/wapiParkingStudy/api/ParkingTransaction?from='+start_date+'&to=' + end_date
+
+
+req = urllib2.urlopen(url)
+csvFile = csv.reader(req)
+
+#DataID Key()                           Text               System unique identifier  
+#MeterCode(0)                   Text               Unique identifier of the pay station
+#TransactionID (1)                Text               The unique transaction identifier 
+#TransactionDateTime (2)    Text               The date and time of the transaction as recorded 
+#Amount(3)                          Text               The amount of the transactions in dollars 
+#User Number (4)                 Text               Equals to 1; can be disregarded   
+#PaymentMean (5)              Text               Type of payment (Coin, Credit, Phone) 
+#PaidDuration (6)                Text               The total amount of time in seconds this payment represents. This field may include an extra 2 minute grace period or the prepayment hours before paid hours begin.  Some older machines can only report time in 5 minute increments.  (Number) 
+#Elementkey  (7)                 Text               Unique identifier for the city street segment where the pay station is located 
+#Year  (8)                             Text               The year of the transaction as recorded (derived from TransactionDateTime)  
+#Month (9)                           Text               The month of the transaction as recorded (derived from TransactionDateTime) 
+
+#creates a dictionary with DataId as a key,and the rest of the elements are in a list. 
+dictionary ={}
+headerFields = csvFile.next() #throwing header away
+for row in csvFile:
+   dictionary[row[0]] = [row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]]
+print (dictionary)
+
