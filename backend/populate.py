0025dc55e93ec199ae72464edaa0b0204ce62fb5
Daniel Ng <daniel@medium-one.com>
2016-02-05 22:59:52 -0800

    renamed rds cluster AGAIN
    Former-commit-id: 4497d457d3afbdbd987e980cfb8469761cc86c70

--- backend/populate.py
+++ backend/populate.py
@@ -3,7 +3,7 @@ import marshal
 import pickle
 import os
 
-db = MySQLdb.connect(host="parking-sql.c9q5edmigsud.us-west-2.rds.amazonaws.com", port=3306, user=os.environ.get('RDS_USERNAME'), passwd=os.environ.get('RDS_PASSWORD'), db="parking")
+db = MySQLdb.connect(host="parking.c9q5edmigsud.us-west-2.rds.amazonaws.com", port=3306, user=os.environ.get('RDS_USERNAME'), passwd=os.environ.get('RDS_PASSWORD'), db="parking")
 cursor = db.cursor()
 
 locations = pickle.load(open('../experiments/datastore/avgblockfacelocs.pickle', 'rb'))
