b2d73d9e10e77997770f28630bd6f6f98284498d
Daniel Ng <daniel@medium-one.com>
2016-02-07 19:46:15 -0800

    script to be run on crontab, pull data from the day before
    Former-commit-id: 0e402d3d9188c830149261ecd6e1eb40dd1560d2

--- experiments/pullSDOtdata.py
+++ experiments/pullSDOtdata.py
@@ -2,13 +2,15 @@ import marshal
 import urllib2
 import csv
 import datetime
+import MySQLdb
+import os
 
 """""""""
 
 parses and serializes data from SDOT API, one week at a time
 
 """""""""
-
+'''
 startDate = datetime.datetime(2015, 1, 1)
 for x in xrange(52):
     transactions = {}
@@ -19,7 +21,6 @@ for x in xrange(52):
     req = urllib2.urlopen(url)
     csvFile = csv.reader(req)
 
-    '''
     DataID Key()                           Text               System unique identifier  
     MeterCode(0)                   Text               Unique identifier of the pay station
     TransactionID (1)                Text               The unique transaction identifier 
@@ -31,7 +32,6 @@ for x in xrange(52):
     Elementkey  (7)                 Text               Unique identifier for the city street segment where the pay station is located 
     Year  (8)                             Text               The year of the transaction as recorded (derived from TransactionDateTime)  
     Month (9)                           Text               The month of the transaction as recorded (derived from TransactionDateTime) 
-    '''
 
     keys = csvFile.next()
 
@@ -43,3 +43,21 @@ for x in xrange(52):
     startDate = endDate + datetime.timedelta(days=1)
     marshal.dump(transactions, outputFile)
     outputFile.close()
+'''
+
+db = MySQLdb.connect(host="parking.c9q5edmigsud.us-west-2.rds.amazonaws.com", port=3306, user=os.environ.get('RDS_USERNAME'), passwd=os.environ.get('RDS_PASSWORD'), db="parking")
+cursor = db.cursor()
+end_date = datetime.datetime.now()
+start_date = end_date - datetime.timedelta(days=1)
+url = 'http://web6.seattle.gov/SDOT/wapiParkingStudy/api/ParkingTransaction?from='+ start_date.strftime('%m%d%Y') + '&to=' + end_date.strftime('%m%d%Y')
+req = urllib2.urlopen(url)
+csvFile = csv.reader(req)
+
+keys = csvFile.next()
+for row in csvFile:
+    query = 'INSERT INTO transactions (data_id, meter_code, transaction_id, timestamp, amount, payment_mean, duration, element_key) VALUES ('\
+            + row[0] + ', ' + row[1] + ', ' + row[2] + ', "' + datetime.datetime.strptime(row[3], '%d/%m/%Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S') + '", ' + row[4] + ', "' + row[6] + '", ' + row[7] + ', ' + row[8] + ')' 
+    cursor.execute(query)
+
+db.commit()
+db.close()
