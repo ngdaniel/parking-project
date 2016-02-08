8f408865842199e1b1bb0329a49ccd4eff25df36
Kyle Lee <kylekclee@gmail.com>
2016-02-08 13:56:53 -0800

    added in infowindows when clicking payStations
    Former-commit-id: e194ecd304f0e3d5ac59b5b556d9838ca70c2d19

--- backend/app/static/databaseMap.js
+++ backend/app/static/databaseMap.js
@@ -33,7 +33,7 @@ String.prototype.format = function () {
 
     var map;
     var markersList = [];
-
+    var infoWindowList = [];
     //Creates map over seattle and adds click listener
     window.initMap = function () {
         map = new google.maps.Map(document.getElementById('map'), {
@@ -52,22 +52,40 @@ String.prototype.format = function () {
              clearMap();
              searchRadius = $('input[name="radius"]').val();
              if(searchRadius == 0){searchRadius = .25}
+             //Queries python API for datapoints
              $.getJSON($SCRIPT_ROOT + '/paystations_in_radius', {
                 latitude: latLng.lat,
                 longitude: latLng.lng,
                 radius: searchRadius
                 }, function(data) {
                 markAndCircle(latLng,searchRadius,map);
+                //Loop over each datapoint(payStation)
                 $.each(data.result, function(index){
                     payStationItem = data.result[index]
+                    idNumber=payStationItem[0];
                     meterLat =payStationItem[1];
                     meterLong = payStationItem[2];
+                    meterMaxOcc =payStationItem[3];
+                    distance =payStationItem[4];
+                    //Adds marker and infowindow  + click listners for each payStation
                     var marker = new google.maps.Marker({
                         position : new google.maps.LatLng(meterLat,meterLong),
                         map: map,
                         icon:$SCRIPT_ROOT + '/static/parking-meter-export.png' 
                      });
+                    //TODO: Make a better looking Info window
+                     infoWindowContent = '<p>parkingMeter {} has a max capacity {} and is {} km away from destination </p>'.format(idNumber,meterMaxOcc,distance); 
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: infoWindowContent
+                     });
+                     marker.addListener('click',function(){
+                        for(var i = 0; i <infoWindowList.length; i ++){
+                             infoWindowList[i].close();
+                    }
+                        infoWindow.open(map,marker);
+                    });
                      markersList.push(marker);
+                     infoWindowList.push(infoWindow);
                 });
               });
               return false;
@@ -78,14 +96,15 @@ String.prototype.format = function () {
                 for(var i = 0; i <markersList.length; i ++){
                     markersList[i].setMap(null);
                     }
+                    infoWindowList = [];
                     markersList = [];
             }
         //takes a latLong object , radius , and map 
         //draws a maker and circle around point
        function markAndCircle(searchCoord,searchRadius,map){
             var marker = new google.maps.Marker({
-                    position :searchCoord ,
-                    map: map,
+                position :searchCoord ,
+                map: map,
                  });
             var cityCircle = new google.maps.Circle({
               strokeColor: '#FF0000',
@@ -98,6 +117,7 @@ String.prototype.format = function () {
               radius: searchRadius*1000 //radius is in meters
               });
             markersList.push(marker);
+            //Adds circle into markerList so that it gets cleared at the same time
             markersList.push(cityCircle);
            }
     }
