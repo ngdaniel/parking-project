3c2abc396880346d803c18b242e57e2d60e156c8
Jake G <omonoid@uw.edu>
2016-02-08 18:12:39 -0800

    code to generate html form usedblocks.p
    Former-commit-id: b9b2cc71be8be8933218ba50b11654e90775e279

--- experiments/show_all_blocks.py
+++ experiments/show_all_blocks.py
@@ -15,17 +15,18 @@ def draw_blocks(coords, clr):
 gmap=gmplot.GoogleMapPlotter(47.6097,-122.3331,13)
 
 # open block coords
-locs=pickle.load(open('datastore/blockfacelocs.p','rb')) # load location data
+usedblocks=pickle.load(open('datastore/usedblocks.p','rb')) # load location data
 allblocks = marshal.load(open('datastore/allblockfaces.m', 'rb'))
 
-# create numpy vector
-clusterlocs=np.asarray(locs.values())
-blocks = np.asanyarray(allblocks.values())
-print 'Drawing %d red blocks, %d green blocks' % (np.size(clusterlocs,0), np.size(blocks,0))
-draw_blocks(clusterlocs, 'red')
-draw_blocks(blocks, 'blue')
+# create numpy vector and draw
+used = np.asanyarray(usedblocks.values())
+all = np.asanyarray(allblocks.values())
+print 'Drawing %d (used) red blocks, %d (all) green blocks' % (np.size(used,0), np.size(all,0))
+draw_blocks(all, 'blue')
+draw_blocks(used, 'red')
 
-fname = 'blueblocks.html'
+# save html
+fname = 'used_vs_all.html'
 path = 'html/'
 gmap.draw(path + fname)
 webbrowser.open('file://' + os.path.realpath(path + fname))
