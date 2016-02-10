b3dbd330e27602c43619d47a65012da5eeb2e814
Daniel Ng <daniel@medium-one.com>
2016-02-09 19:18:38 -0800

    urllib2 is awful, switched to requests
    Former-commit-id: 78a2c71dd80a1d068c9705e7c31840268c953d79

--- experiments/daily_pull.py
+++ experiments/daily_pull.py
@@ -1,24 +1,27 @@
-import urllib2
+import requests
 import csv
 import datetime
 import pytz
 import MySQLdb
 import os
 import logging
+import traceback
 
 db = MySQLdb.connect(host="parking.c9q5edmigsud.us-west-2.rds.amazonaws.com", port=3306, user='parking', passwd='sdotpark1ng', db="parking")
 cursor = db.cursor()
 
-logging.basicConfig(filename='/home/ec2-user/pull.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.basicConfig(filename='pull.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-date = datetime.datetime.now(tz=pytz.timezone('US/Pacific')) - datetime.timedelta(days=1)
+date = datetime.datetime.now(tz=pytz.timezone('US/Pacific')) - datetime.timedelta(days=8)
 url = 'http://web6.seattle.gov/SDOT/wapiParkingStudy/api/ParkingTransaction?from='+ date.strftime('%m%d%Y') + '&to=' + date.strftime('%m%d%Y')
-req = urllib2.urlopen(url)
-csvFile = csv.reader(req)
+r = requests.get(url)
+reader = csv.reader(r.text.splitlines())
 inserted = 0
 duplicates = 0
-keys = csvFile.next()
-for row in csvFile:
+keys = reader.next()
+for row in reader:
+    if '\"' in row[6]:
+        row[6] = "NULL"
     query = 'INSERT INTO transactions (data_id, meter_code, transaction_id, timestamp, amount, payment_mean, duration, element_key) VALUES ('\
             + row[0] + ', ' + row[1] + ', ' + row[2] + ', "' + datetime.datetime.strptime(row[3], '%m/%d/%Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S') + '", ' + row[4] + ', "' + row[6] + '", ' + row[7] + ', ' + row[8] + ')' 
     try: 
@@ -26,6 +29,9 @@ for row in csvFile:
         inserted += 1
     except MySQLdb.IntegrityError:
         duplicates += 1
+    except MySQLdb.ProgrammingError:
+        logging.error(query)
+        logging.error(traceback.format_exc())
 
 db.commit()
 db.close()
